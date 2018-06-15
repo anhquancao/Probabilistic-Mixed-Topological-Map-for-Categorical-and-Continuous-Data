664c0a5f94187de59a7c01c630300f8297649369
caoquan95 <aquancva@gmail.com>
2018-06-15 22:16:50 +0700

    read csv file

--- src/main/scala/com/quan/main/Main.scala
+++ src/main/scala/com/quan/main/Main.scala
@@ -7,8 +7,9 @@ object Main {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
 
-    val r = Reader.read("src/resources/mfeat-kar.txt", "[ \t]+")
-
+    val r = Reader.read("src/resources/mfeat-kar.txt", "[ \t]+").map(_.map(_.toDouble))
+    val b = Reader.read("src/resources/digits.csv", ",").map(_.map(_.toInt))
     println("r size: " + r.count() + " x " + r.take(1)(0).size)
+    println("b size: " + b.count() + " x " + b.take(1)(0).size)
   }
 }
--- src/main/scala/com/quan/main/Reader.scala
+++ src/main/scala/com/quan/main/Reader.scala
@@ -4,11 +4,11 @@ import com.quan.context.Context
 import org.apache.spark.rdd.RDD
 
 object Reader {
-  def read(fileName: String, delimiter: String): RDD[Array[Double]] = {
+  def read(fileName: String, delimiter: String): RDD[Array[String]] = {
     val sc = Context.getSparkContext;
     val textFile = sc.textFile(fileName)
     val r = textFile.map((lines) => {
-      lines.trim().split(delimiter).map((str) => str.toDouble)
+      lines.trim().split(delimiter)
     })
     r
   }
